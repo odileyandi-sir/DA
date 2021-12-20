d837a98efbc02567f5b6bfcd5f4ee8acca2a5696
odileyandi-sir <87127794+odileyandi-sir@users.noreply.github.com>
2021-12-20 22:05:13 +0530

    Create SummarizingStats.R

---
+++ SummarizingStats.R
@@ -0,0 +1,18 @@
+summary(iris)
+
+str(mtcars)
+
+head(mtcars)
+
+tail(mtcars)
+
+names(mtcars)
+
+nrow(mtcars)
+
+aggregate(Sepal.Length~Species,iris,mean)
+
+fix(iris)
+
+sepalsub<- subset(iris,Sepal.Length>7)
+ sepalsub
